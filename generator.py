507ae97b5f03cc28615584c74ebf41d0bfde02cb
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-03 12:30:15 +0300

    added generator function

---
+++ generator.py
@@ -0,0 +1,20 @@
+def element_length_calculation(list_with_words: list[str, ...]):
+    list_with_len = []
+    for i in list_with_words:
+        if not isinstance(i, str):
+            raise ValueError('The list must contain only strings')
+
+        elif isinstance(i, str):
+            list_with_len.insert(0, len(i))
+
+    return list_with_len
+
+
+def generator(list_with_words: list[str, ...]) -> list[int, ...]:
+    list_with_len = element_length_calculation(list_with_words)
+
+    for j in list_with_len:
+        yield j
+
+
+test_list = generator(['one', 'two', 'three', 'long number'])
