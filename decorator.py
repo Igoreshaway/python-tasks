5020c616475522b043da6b8931723ff27bfcee25
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-04 11:41:57 +0300

    added 2 decorators to calculate the time of the function

--- decorator.py
+++ decorator.py
@@ -1,23 +1,56 @@
 import time
+from datetime import datetime
 from functools import wraps
 
 
-def timeit(func):
+def work_time_count_perf_counter(func):
     @wraps(func)
-    def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
-        result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        total_time = end_time - start_time
-        print(f'Start time {start_time}')
-        print(f'End time {end_time}')
-        print(f'Total time {total_time:.2f} seconds')
-        return result
+    def work_time_count_perf_counter_wrapper(*args, **kwargs):
+        start_time_perf = time.perf_counter()
+        result_perf = func(*args, **kwargs)
+        end_time_perf = time.perf_counter()
+        total_time_perf = end_time_perf - start_time_perf
+        print(f'Start time {start_time_perf:.1f}')
+        print(f'End time {end_time_perf:.1f}')
+        print(f'Total time {total_time_perf:.1f} seconds')
+        return result_perf
 
-    return timeit_wrapper
+    return work_time_count_perf_counter_wrapper
 
 
-@timeit
+def work_time_count_datetime(func):
+    @wraps(func)
+    def work_time_count_datetime_wrapper(*args, **kwargs):
+        start_time_datetime = datetime.now()
+        result_datetime = func(*args, **kwargs)
+        end_time_datetime = datetime.now()
+        total_time_datetime = end_time_datetime - start_time_datetime
+        print(f"Start time {start_time_datetime}")
+        print(f"End time {end_time_datetime}")
+        print(f"Total time {total_time_datetime}")
+        return result_datetime
+
+    return work_time_count_datetime_wrapper
+
+
+def work_time_count_monotonic(func):
+    @wraps(func)
+    def work_time_count_monotonic_wrapper(*args, **kwargs):
+        start_time_monotonic = time.monotonic()
+        result_monotonic = func(*args, **kwargs)
+        end_time_monotonic = time.monotonic()
+        total_time_monotonic = end_time_monotonic - start_time_monotonic
+        print(f"Start time {start_time_monotonic:.1f}")
+        print(f"End time {end_time_monotonic:.1f}")
+        print(f"Total time {total_time_monotonic:.1f}")
+        return result_monotonic
+
+    return work_time_count_monotonic_wrapper
+
+
+@work_time_count_perf_counter
+@work_time_count_datetime
+@work_time_count_monotonic
 def test_function(num: int):
     total = sum((x for x in range(0, num ** 3)))
     return total
