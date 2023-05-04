226201604da9c7c1c1cfc171eaef6ddd2bb3f9a3
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-04 10:11:29 +0300

    renamed variable conv_string to convertible_string

--- string_to_int.py
+++ string_to_int.py
@@ -1,18 +1,18 @@
 
 
-def convert_string_to_int(conv_str: str) -> int:
+def convert_string_to_int(convertible_string: str) -> int:
     """
     This function converts a string consisting of numbers to a number
 
     :param conv_str: string to be converted
     :return: this function returns the received number
     """
-    if not conv_str.isnumeric():
+    if not convertible_string.isnumeric():
         raise ValueError('Please enter a string consisting of numbers only')
 
-    if conv_str.isnumeric():
+    if convertible_string.isnumeric():
         result = 0
-        for c in conv_str:
+        for c in convertible_string:
             result = result * 10 + ord(c) - ord('0')
 
         return result
