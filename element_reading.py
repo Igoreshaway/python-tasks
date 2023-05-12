29230ce716dc8cc22dd8b616dbc418a0565eadbc
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-12 17:58:22 +0300

    ref: rename variable

--- element_reading.py
+++ element_reading.py
@@ -4,16 +4,15 @@ PATH_TO_FILE = './file_el'
 
 
 def get_common_chars(path_to_file: str) -> list[str]:
-    with open(PATH_TO_FILE, 'r') as file:
+    with open(path_to_file, 'r') as file:
         name_and_count_chars = defaultdict(int)
         for line in file:
-            line_without_break_char = line.replace('\n', '')
-            for char in line_without_break_char:
+            for char in line.replace('\n', ''):
                 name_and_count_chars[char] += 1
-        max_value_char_repetitions = max(name_and_count_chars.values())
+        max_char_repetitions = max(name_and_count_chars.values())
         common_chars = []
         for char, number_of_repetitions in name_and_count_chars.items():
-            if number_of_repetitions == max_value_char_repetitions:
+            if number_of_repetitions == max_char_repetitions:
                 common_chars.append(char)
         return common_chars
 
