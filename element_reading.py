59d93d0d0ee63c47b00d330b02f7cb6a0d6b6b5c
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-12 17:41:59 +0300

    ref: rename variable

--- element_reading.py
+++ element_reading.py
@@ -10,10 +10,10 @@ def get_common_chars(path_to_file: str) -> list[str]:
             line_without_break_char = line.replace('\n', '')
             for char in line_without_break_char:
                 name_and_count_chars[char] += 1
-        mex_value_char_repetitions = max(name_and_count_chars.values())
+        max_value_char_repetitions = max(name_and_count_chars.values())
         common_chars = []
         for char, number_of_repetitions in name_and_count_chars.items():
-            if number_of_repetitions == mex_value_char_repetitions:
+            if number_of_repetitions == max_value_char_repetitions:
                 common_chars.append(char)
         return common_chars
 
