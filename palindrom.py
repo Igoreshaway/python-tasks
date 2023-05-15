fb86dac288854c90d603e3a7eef0b264b35896be
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-15 10:30:00 +0300

    ref: refactor code

--- palindrom.py
+++ palindrom.py
@@ -1,7 +1,4 @@
 def check_palindrome(check_string: str) -> bool:
-    reversed_string = ''.join(reversed(check_string))
-    if check_string == reversed_string:
-        return True
-    return False
+    return check_string == ''.join(reversed(check_string))
 
 
