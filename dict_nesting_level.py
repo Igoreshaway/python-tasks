6d5bccef619c0f1d3b44f84a2fca16ab9ecc8aa2
Ireal-ai <pesotskygoscha@yandex.ru>
2023-05-16 12:06:08 +0300

    ref: refactored code

--- dict_nesting_level.py
+++ dict_nesting_level.py
@@ -10,35 +10,20 @@ content = {
 }
 
 
-# def get_nesting_level(content: dict, result: dict = None, nesting_level: int = 0) -> dict[str, int]:
-#     if result is None:
-#         result = {}
-#     for_unpack = [content]
-#     get_elem_key = for_unpack[0].keys()
-#
-#     if len(get_elem_key) > 1:
-#         for i in get_elem_key:
-#             result[i] = nesting_level
-#     else:
-#         result[str(*get_elem_key)] = nesting_level
-#
-#     unpacing = for_unpack[0].pop(*get_elem_key)
-#     nesting_level += 1
-#     if unpacing is None:
-#         return result
-#     return get_nesting_level(unpacing, result, nesting_level)
+def get_nesting_level(
+        source_dict: dict, nesting_level: int = 0
+) -> dict[str, int]:
 
-def get_nesting_level(source_dict: dict, dict_with_number_nesting_level: dict = None, nesting_level: int = 0) -> dict[
-    str, int]:
-    if dict_with_number_nesting_level is None:
-        dict_with_number_nesting_level = {}
+    keys_and_numbers_nesting_levels = {}
 
     for key, value in source_dict.items():
-        dict_with_number_nesting_level[key] = nesting_level
+        keys_and_numbers_nesting_levels[key] = nesting_level
         if value:
-            get_nesting_level(value, dict_with_number_nesting_level, nesting_level=nesting_level + 1)
-    return dict_with_number_nesting_level
+            keys_and_numbers_nesting_levels.update(
+                get_nesting_level(value, nesting_level=nesting_level + 1)
+            )
+    return keys_and_numbers_nesting_levels
 
 
-key_with_nested_level = get_nesting_level(content)
-print(key_with_nested_level)
+keys_with_nested_levels = get_nesting_level(content)
+print(keys_with_nested_levels)
